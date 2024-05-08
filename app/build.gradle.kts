b7b68f2e480cd23dec79a739864aea6791dee070
alan.bastos.glo <alan.bastos.glo>
2024-05-07 22:46:45 -0300

    adding glide and retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //retrofit
+    val retrofitVersion = "2.11.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+
+    //glide
+    val glideVersion = "4.16.0"
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+
 }
