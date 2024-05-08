10d06bbd1792902df2d1090e7596f30415189262
AlanBastos <alan.bastos.glo>
2024-05-08 10:07:03 -0300

    repository adapter

--- app/src/main/java/com/example/githubchallenge/view/RepositoryAdapter.kt
+++ app/src/main/java/com/example/githubchallenge/view/RepositoryAdapter.kt
@@ -1,31 +1,52 @@
 package com.example.githubchallenge.view
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.githubchallenge.R
+import com.example.githubchallenge.databinding.ItemRepositoryBinding
 import com.example.githubchallenge.model.Repository
 
 
 class RepositoryAdapter(private val repositories: List<Repository>) :
     RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder>() {
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): RepositoryAdapter.RepositoryViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) :
+            RepositoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemRepositoryBinding.inflate(inflater, parent,false)
+        return RepositoryViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RepositoryAdapter.RepositoryViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) {
+        holder.bind(repositories[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return repositories.size
     }
 
-    class RepositoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class RepositoryViewHolder(private val binding: ItemRepositoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(repository: Repository) {
+            binding.repository = repository
+            binding.executePendingBindings()
+
+            binding.tvUsername.text = repository.owner.login
+            binding.tvRepositoryName.text = repository.name
+            binding.tvRepositoryDescription.text = repository.description
+            binding.tvStars.text = repository.stargazers_count.toString()
+            binding.tvFork.text = repository.forks_count.toString()
+
+            Glide.with(itemView.context)
+                .load(repository.owner.avatar_url)
+                .circleCrop()
+                .placeholder(R.drawable.baseline_person_24)
+                .into(binding.avatar)
+
+        }
     }
 
 }
