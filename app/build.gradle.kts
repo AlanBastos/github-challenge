9daf06c86060274dd16c28aeee2445f6ad70fdea
AlanBastos <alan.bastos.glo>
2024-05-07 23:11:25 -0300

    fix projet name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.githubchallenge"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.githubchallenge"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
